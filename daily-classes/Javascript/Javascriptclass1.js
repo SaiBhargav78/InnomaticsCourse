049ae1083c5b98b7fab203ccc1729e4ce610c72c
SaiBhargav78 <satyabhargavvg@gmail.com>
2024-07-12 13:33:03 +0530

    Js file modified

--- daily-classes/Javascript/Javascriptclass1.js
+++ daily-classes/Javascript/Javascriptclass1.js
@@ -1,64 +1,120 @@
-// var age = 12;
-// console.log(age);
+// Data Types Introduction
+console.log();
+// String
+let Course = "Full Stack Development";
+console.log(typeof Course, Course);
+console.log();
 
-// console.log(age);
-// var age = 12;
+// Number
+let batch_number = 38;
+console.log(typeof batch_number,batch_number);
+console.log();
 
-// console.log(age);
+// Boolean
+let morning_batch = batch_number===38;
+console.log(typeof morning_batch,morning_batch);
+console.log();
+
+// undefined
+let Un_defined;
+console.log(typeof Un_defined,Un_defined);
+console.log();
+
+// Null
+let nullvar = null;
+console.log(typeof nullvar,nullvar);
+console.log();
+
+// Variables for operators
 let age = 12;
 let height = 6;
+console.log("age = "+age,"height = "+height);
+console.log();
 
+// Mathematical Operators
 // Addition
 console.log("Addition:",age+height);
+console.log();
 
 // Subtraction
 console.log("Subtraction:",age-height);
+console.log();
 
 // Multiplication
 console.log("Multiplication:",age*height);
+console.log();
 
 // Division
 console.log("Division:",age/height);
+console.log();
 
 // Modulus
 console.log("Modulus:",age%height);
+console.log();
 
+// Comparator operators
 // Age != height
 console.log("Age != height:",age!=height);
+console.log();
 
 // Age > height
 console.log("Age > height:",age>height);
+console.log();
 
 // Age < height
 console.log("Age < height:",age<height);
+console.log();
 
 // Age == height
 console.log("Age == height:",age==height);
+console.log();
+
+// String == number
+let string = "2";
+let number = 2;
+console.log("string = "+string,"number = "+number);
+console.log();
+
+console.log("String == number:",string == number);
+console.log();
+
+// String === number
+console.log("String === number:",string === number);
+console.log();
 
 // Age >= height
 console.log("Age >= height:",age>=height);
 
 // Age <= height
 console.log("Age <= height:",age<=height);
+console.log();
 
+// Increment and Decrement
 // preincrement
 console.log("Preincrement(++Age):",++age);
 console.log("Age:",age);
+console.log();
 
 // postincrement
 console.log("Postincrement(Age++):",age++);
 console.log("Age:",age);
+console.log();
 
 // predecrement
 console.log("Predecrement(--Age):",--age);
 console.log("Age:",age);
+console.log();
 
 // postdecrement
 console.log("Postdecrement(Age--):",age--);
 console.log("Age:",age);
+console.log();
 
+// And OR operators
 // And operator
 console.log("Age && height:",age&&height);
+console.log();
 
 // Or Operator
 console.log("Age || height:",age||height);
+console.log();
