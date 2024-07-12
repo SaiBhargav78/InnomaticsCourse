73d0cb2c037fab9de2121ec0ee7f656c90805fa5
SaiBhargav78 <satyabhargavvg@gmail.com>
2024-07-12 11:06:02 +0530

    Javascript Folder

---
+++ daily-classes/Javascript/Javascriptclass1.js
@@ -0,0 +1,64 @@
+// var age = 12;
+// console.log(age);
+
+// console.log(age);
+// var age = 12;
+
+// console.log(age);
+let age = 12;
+let height = 6;
+
+// Addition
+console.log("Addition:",age+height);
+
+// Subtraction
+console.log("Subtraction:",age-height);
+
+// Multiplication
+console.log("Multiplication:",age*height);
+
+// Division
+console.log("Division:",age/height);
+
+// Modulus
+console.log("Modulus:",age%height);
+
+// Age != height
+console.log("Age != height:",age!=height);
+
+// Age > height
+console.log("Age > height:",age>height);
+
+// Age < height
+console.log("Age < height:",age<height);
+
+// Age == height
+console.log("Age == height:",age==height);
+
+// Age >= height
+console.log("Age >= height:",age>=height);
+
+// Age <= height
+console.log("Age <= height:",age<=height);
+
+// preincrement
+console.log("Preincrement(++Age):",++age);
+console.log("Age:",age);
+
+// postincrement
+console.log("Postincrement(Age++):",age++);
+console.log("Age:",age);
+
+// predecrement
+console.log("Predecrement(--Age):",--age);
+console.log("Age:",age);
+
+// postdecrement
+console.log("Postdecrement(Age--):",age--);
+console.log("Age:",age);
+
+// And operator
+console.log("Age && height:",age&&height);
+
+// Or Operator
+console.log("Age || height:",age||height);
